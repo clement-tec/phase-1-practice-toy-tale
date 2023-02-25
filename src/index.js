a854550c74dc52e185b10034431ae736619d253b
clement senou <clementsenou74@gmail.com>
2023-02-24 20:05:02 -0600

    complet

--- src/index.js
+++ src/index.js
@@ -11,5 +11,79 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       toyFormContainer.style.display = "none";
     }
-  });
+  });})
+
+const toyCollection = document.querySelector("#toy-collection");
+
+fetch('http://localhost:3000/toys')
+.then(response => response.json())
+.then(data =>
+   data.forEach(renderToy))
+
+const renderToy = (toy) => {
+  console.log(toy);
+  const toyCard = document.createElement('div');
+  toyCard.className = "card";
+  toyCollection.append(toyCard);
+
+  const h2 = document.createElement('name');
+  h2.textContent = toy.name 
+  toyCard.append(h2);
+
+  const img = document.createElement('img');
+  img.src = toy.image
+  img.className = "toy-avatar";
+  toyCard.append(img);
+
+  const TheP = document.createElement("p");
+  TheP.textContent = `${toy.likes} likes`;
+  toyCard.append(TheP);
+
+  const button = document.createElement("button");
+  button.className = "like-btn";
+  button.textContent = "❤️";
+  toyCard.append(button);
+
+  button.addEventListener("click", (e) => {
+    
+    const newLike = toy.likes + 1
+    toy.likes = newLike
+    TheP.textContent = `${newLike} likes`;
+
+    fetch(`http://localhost:3000/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+        accept: "application/json",
+      },
+      body: JSON.stringify({
+        likes: newLike,
+      }) 
+  })
+});
+};
+
+const form = document.querySelector('form');
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = e.target.name.value;
+  const image = e.target.image.value;
+  const newToy = {
+  name: name,
+  image: image,
+  likes: 0,
+  }
+
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      accept: "application/json"
+    },
+    body: JSON.stringify(newToy)
+  })
+  .then((response) => response())
+  .then((toy) => {
+    renderToy(toy);
+  })
 });
